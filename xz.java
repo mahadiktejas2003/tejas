b8087209527505b818194ac1bd275cf72cb24885
mahadiktejas2003 <118093143+mahadiktejas2003@users.noreply.github.com>
2023-04-14 22:22:42 +0530

    Add files via upload

---
+++ xz.java
@@ -0,0 +1,37 @@
+//UPCASTING
+/*
+ Upcasting is the typecasting of a child object to a parent object. Upcasting can be done implicitly. Upcasting gives us the flexibility to access the parent class members but it is not possible to access all the child class members using this feature. Instead of all the members, we can access some specified members of the child class. For instance, we can access the overridden methods.
+ */
+class Phone{
+    public void showTime(){
+        System.out.println("Time is 8 am");
+    }
+    public void on(){
+        System.out.println("Turning on Phone...");
+    }
+}
+
+class SmartPhone extends Phone{
+    public void music(){
+        System.out.println("Playing music...");
+    }
+    public void on(){
+        System.out.println("Turning on SmartPhone...");
+    }
+}
+public class xz {
+    public static void main(String[] args) {
+        // Phone obj = new Phone(); // Allowed
+        // SmartPhone smobj = new SmartPhone(); // Allowed
+        // obj.name();
+
+        Phone obj = new SmartPhone(); // Yes it is allowed
+        // SmartPhone obj2 = new Phone(); // Not allowed
+
+        obj.showTime();
+        obj.on();
+        // obj.music(); Not Allowed
+
+
+    }
+}
